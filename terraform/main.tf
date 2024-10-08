029cedcd9994f021f45df2f33907b432f7076603
root <root@ip-172-31-7-12.eu-central-1.compute.internal>
2024-10-08 14:26:15 +0000

    dq

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-central-1"
 }
 
 resource "aws_vpc" "my_vpc" {
@@ -13,7 +13,7 @@ resource "aws_vpc" "my_vpc" {
 resource "aws_subnet" "my_subnet" {
   vpc_id            = aws_vpc.my_vpc.id
   cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-central-1"
 
   tags = {
     Name = "my_subnet"
@@ -43,7 +43,7 @@ resource "aws_security_group" "my_sg" {
 }
 
 resource "aws_instance" "my_ec2" {
-  ami                    = "ami-0c55b159cbfafe1f0" # Укажи актуальный AMI для региона
+  ami                    = "ami-0084a47cc718c111a" # Укажи актуальный AMI для региона
   instance_type         = "t2.micro"
   subnet_id             = aws_subnet.my_subnet.id
   vpc_security_group_ids = [aws_security_group.my_sg.id]
